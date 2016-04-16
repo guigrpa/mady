6ad689e0394b300c751fa16833bb158b2024d963
G. Grau <guigrpa@gmail.com>
2016-04-16 08:39:17 +0200

    Add _t implementation

--- src/exampleLocales.js
+++ src/exampleLocales.js
@@ -1,7 +1,12 @@
+import _t from './translate';
+import locales from '../locales/es-ES';
+
+_t.setLocales(locales);
+
 // Some examples of strings that will be detected:
-_t("exampleContext_Example string");
-_t("exampleContext_Example string");
-_t("exampleContext_Example string");
-_t("exampleContext_Example string");
-_t("exampleContext_Number of items: {NUM}", NUM);
-_t("exampleContext_Extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long string");
+console.log(_t("exampleContext_Example string"));
+console.log(_t("exampleContext_Example string"));
+console.log(_t("exampleContext_Example string"));
+console.log(_t("exampleContext_Example string"));
+console.log(_t("exampleContext_Number of items: {NUM}", { NUM: 5 }));
+console.log(_t("exampleContext_Extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long extremely long string"));
---
+++ src/translate.js
@@ -0,0 +1,23 @@
+let _locales = {};
+
+function translate(locales, key, data) {
+  const fnTranslate = locales[key];
+  let out;
+  if (fnTranslate) {
+    out = fnTranslate(data);
+  } else {
+    out = key;
+    const contextSeparatorIndex = out.indexOf('_');
+    if (contextSeparatorIndex >= 0) {
+      out = out.substring(contextSeparatorIndex+1, out.length);
+    }
+  }
+  out = out.trim();
+  return out;
+}
+
+function _t(key, data) { return translate(_locales, key, data); }
+
+_t.setLocales = function(locales) { _locales = locales; }
+
+export default _t;
