f9c73d61ab243f1beb7f1eb531d97a614efb17ca
G. Grau <guigrpa@gmail.com>
2016-09-27 23:40:05 +0200

    Minor

--- src/locales/ca.js
+++ src/locales/ca.js
@@ -33,7 +33,7 @@ return {
   "c29tZUNvbnRleHRfSGVsbG8sIHtOQU1FfSE=": function(d) { return "Hello, " + d.NAME + "!"; },
   "c29tZUNvbnRleHRfSGVsbG8ge05BTUV9LCB5b3UgaGF2ZSB7VU5SRUFEX0NPVU5ULCBudW1iZXJ9IHtVTlJFQURfQ09VTlQsIHBsdXJhbCwgb25lIHttZXNzYWdlfSBvdGhlciB7bWVzc2FnZXN9fQ==": function(d) { return "Hello " + d.NAME + ", you have " + fmt.number(d.UNREAD_COUNT, ["ca"]) + " " + plural(d.UNREAD_COUNT, 0, pluralFuncs.ca, { one: function() { return "message";}, other: function() { return "messages";} }); },
   "c29tZUNvbnRleHRfPGk+SGk8L2k+IDxiPntOQU1FfTwvYj4h": function(d) { return "<i>Hi</i> <b>" + d.NAME + "</b>!"; },
-  dG9vbHRpcF9Db252ZXJ0IHRyYW5zbGF0aW9ucyB0byBKYXZhU2NyaXB0IGZpbGVz: function(d) { return "Convertir traduccions a fitxers javaScript"; },
+  dG9vbHRpcF9Db252ZXJ0IHRyYW5zbGF0aW9ucyB0byBKYXZhU2NyaXB0IGZpbGVz: function(d) { return "Convertir traduccions a fitxers JavaScript"; },
   "dG9vbHRpcF9TZXR0aW5ncw==": function(d) { return "Ajustos"; },
   "YnV0dG9uX0RlbGV0ZQ==": function(d) { return "Esborrar"; },
   "YnV0dG9uX1JldmVydA==": function(d) { return "Cancel·lar canvis"; },
