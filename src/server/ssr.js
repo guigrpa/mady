9c57262bc4fe8d0a0d95a1c7b9f84d0e264bfec5
G. Grau <ggrau@indra.es>
2017-07-19 12:43:35 +0200

    Bump React + remove flow-typed

--- src/server/ssr.js
+++ src/server/ssr.js
@@ -2,7 +2,7 @@
 
 import path from 'path';
 import fs from 'fs';
-import Promise from 'bluebird';
+import Promise from 'bluebird'; // eslint-disable-line
 import React from 'react';
 import IsomorphicRelay from 'isomorphic-relay'; // eslint-disable-line
 import ReactDOMServer from 'react-dom/server';
