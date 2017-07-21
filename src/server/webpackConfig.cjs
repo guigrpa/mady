12863f2c31d53bb296b83f9d9eccb147bf790ad7
G. Grau <ggrau@indra.es>
2017-07-21 09:29:43 +0200

    Remove legacy config file

--- src/server/webpackConfig.cjs
+++
@@ -1,5 +0,0 @@
-require('babel-core/register');
-const { addListener } = require('storyboard');
-const consoleListener = require('storyboard-listener-console').default;
-addListener(consoleListener);
-module.exports = require('./webpackConfig');
