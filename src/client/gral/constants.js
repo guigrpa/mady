c3b6928b7abd44c7dc6f63a3b23ce709c9464a08
Guillermo Grau <guillermo.grau@isardsat.cat>
2018-03-15 09:39:17 +0100

    Allow some theming via URL

--- src/client/gral/constants.js
+++ src/client/gral/constants.js
@@ -1,6 +1,16 @@
 // @flow
 
+/* eslint-env browser */
+
 import tinycolor from 'tinycolor2';
+
+// Detect theme
+let THEME;
+try {
+  THEME = new URL(document.location).searchParams.get('theme');
+} catch (err) {
+  /* ignore */
+}
 
 const BASE_COLOR = tinycolor('aliceblue')
   .spin(40)
@@ -8,24 +18,36 @@ const BASE_COLOR = tinycolor('aliceblue')
 const BASE_COLOR2 = tinycolor('aliceblue')
   .spin(10)
   .toHexString();
-const COLORS = {
-  light: BASE_COLOR,
-  lightAlt: BASE_COLOR2,
-  medium: tinycolor(BASE_COLOR)
-    .darken(5)
-    .toHexString(),
-  mediumAlt: tinycolor(BASE_COLOR2)
-    .darken(5)
-    .toHexString(),
-  dark: tinycolor(BASE_COLOR)
-    .darken(10)
-    .toHexString(),
-  darkest: tinycolor(BASE_COLOR)
-    .darken(65)
-    .toHexString(),
-  dim: '#999',
-  accent: 'darkred',
-};
+const COLORS =
+  THEME === 'embedded'
+    ? {
+        light: undefined,
+        lightAlt: BASE_COLOR2,
+        medium: '#f0f0f0',
+        mediumAlt: '#f8f8f8',
+        dark: '#e0e0e0',
+        darkest: '#888',
+        dim: '#999',
+        accent: 'darkred',
+      }
+    : {
+        light: BASE_COLOR,
+        lightAlt: BASE_COLOR2,
+        medium: tinycolor(BASE_COLOR)
+          .darken(5)
+          .toHexString(),
+        mediumAlt: tinycolor(BASE_COLOR2)
+          .darken(5)
+          .toHexString(),
+        dark: tinycolor(BASE_COLOR)
+          .darken(10)
+          .toHexString(),
+        darkest: tinycolor(BASE_COLOR)
+          .darken(65)
+          .toHexString(),
+        dim: '#999',
+        accent: 'darkred',
+      };
 
 const LANG_OPTIONS = [
   { value: 'en', label: 'English' },
@@ -36,4 +58,4 @@ const LANG_OPTIONS = [
 // ==========================================
 // Public API
 // ==========================================
-export { COLORS, LANG_OPTIONS };
+export { THEME, COLORS, LANG_OPTIONS };
