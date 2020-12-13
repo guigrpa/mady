a50d312e332aba92b5c213c5f5b6ab41ee291a0a
G. Grau <ggrau@me.com>
2020-12-13 23:12:08 +0100

    [server] Add example with unused key, missing translation

--- packages/mady-server/locales/es.js
+++ packages/mady-server/locales/es.js
@@ -20,7 +20,5 @@ module.exports = {
   c29tZUNvbnRleHRfe05VTSwgcGx1cmFsLCBvbmV7MSBoYW1idXJnZXJ9IG90aGVyeyMgaGFtYnVyZ2Vyc319: function(d) { return plural(d.NUM, 0, es, { one: "1 hamburguesa", other: number(d.NUM, "NUM") + " hamburguesas" }); },
   "c29tZUNvbnRleHRfTWVzc2FnZSB3aXRoIGEgZml4ZWQgZXJyb3Ige01JU1NJTkdfQlJBQ0tFVH0=": function(d) { return "Mensaje con un error fijo " + d.MISSING_BRACKET; },
   "Q29udGV4dC1sZXNzIG1lc3NhZ2U=": function(d) { return "Mensaje sin contexto"; },
-  "c29tZUNvbnRleHRfTWVzc2FnZSB3aXRoIGNvbnRleHQ=": function(d) { return "Mensaje con contexto"; },
-  "c29tZUNvbnRleHRfTWVzc2FnZSB3aXRoIGVtb2ppIPCfpbA=": function(d) { return "Mensaje con emoji 🥰"; },
-  "c29tZUNvbnRleHRfQSBuZXcgbWVzc2FnZQ==": function(d) { return "Un nuevo mensaje"; }
+  "c29tZUNvbnRleHRfTWVzc2FnZSB3aXRoIGVtb2ppIPCfpbA=": function(d) { return "Mensaje con emoji 🥰"; }
 };
